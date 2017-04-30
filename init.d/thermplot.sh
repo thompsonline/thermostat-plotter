6a56b477faeecb8196daa446955d1d496ebfb750
thompsonline <john.thompson@start-thinking.com>
2017-04-30 15:19:59 -0600

    Changed directory

--- init.d/thermplot.sh
+++ init.d/thermplot.sh
@@ -11,7 +11,7 @@
 ### END INIT INFO
 
 # Change the next 3 lines to suit where you install your script and what you want to call it
-DIR='/home/john/thermostat-plotter'
+DIR='/home/john/thermy/thermostat-plotter'
 DAEMON=$DIR/autoPlot.py
 DAEMON_NAME='thermostatPlotter'
 
